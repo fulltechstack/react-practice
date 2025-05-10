ac165f72c6c567a8a1dd2ad3015b6669cc22d4bc
fulltechstack <fulltechstack@gmail.com>
2025-05-10 15:09:28 +0530

    useState - create a counter and increase counter btn

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,20 @@ import { createPortal } from 'react-dom'
 function App() {
   const testPortal = () => {
     return createPortal(<div>This is test</div>, document.getElementById('root2'));
+  }
 
+  let [count, setCount] = useState(0);
+
+  const clickCountHandler = () => {
+    setCount(prev => prev + 1);
   }
 
   return (
     <>
       {testPortal()}
+
+      {count}
+      <button onClick={clickCountHandler}>Increase</button>
     </>
   )
 }
