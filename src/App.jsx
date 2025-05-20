010ee0941047bb35fbae1f2943e33ef6c084a16a
fulltechstack <fulltechstack@gmail.com>
2025-05-20 12:52:08 +0530

    Fresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { createPortal } from 'react-dom'
-import UseEffectDemo from './demos/UseEffectDemo'
-import UseThemeDemo from './demos/UseThemeDemo'
-import ThemeContext from './context/ThemeContext'
 
 function App() {
-  const testPortal = () => {
-    return createPortal(<div>This is test</div>, document.getElementById('root2'));
-  }
-
-  let [count, setCount] = useState(0);
-  let [theme, setTheme] = useState('light');
-
-
-  const clickCountHandler = () => {
-    setCount(prev => prev + 1);
-  }
-
-  return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div style={{ background: theme === 'light' ? 'black' : 'white', color: theme === 'light' ? 'white' : 'black' }}>
-        {testPortal()}
-
-        {count}
-        <button onClick={clickCountHandler}>Increase</button>
-
-        <UseEffectDemo></UseEffectDemo>
-        <UseThemeDemo></UseThemeDemo>
-      </div>
-    </ThemeContext.Provider>
-  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,5 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <Todo />
-  </Provider>,
+  <App />
 )
