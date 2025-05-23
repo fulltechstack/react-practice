4337ddc4e1e0e1de2f20de1d77a9a11dccb8303c
fulltechstack <fulltechstack@gmail.com>
2025-05-23 12:37:01 -0400

    back to basics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Todo from './Todo.jsx'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <Todo />
-  </Provider>,
+    <App />
 )
